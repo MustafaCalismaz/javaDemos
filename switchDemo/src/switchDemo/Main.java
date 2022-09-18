2fc3007fa9c0095ff4606c618447db3fd9ed5764
alemd <alemd@Computerstafa.home>
2022-09-18 17:20:16 +0300

    "Switch Bloklarıyla Çalışmak" adlı videonun uygulaması

---
+++ switchDemo/src/switchDemo/Main.java
@@ -0,0 +1,30 @@
+package switchDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade = 'B';
+
+		switch (grade) {
+		case 'A':
+			System.out.println("Mükemmel : Geçtiniz");
+			break;
+		case 'B':
+		case 'C':
+			System.out.println("İyi : Geçtiniz");
+			break;
+		case 'D':
+			System.out.println("Fena Değil : Geçtiniz");
+			break;
+		case 'F':
+			System.out.println("Malesef Kaldınız");
+			break;
+
+		default:
+			System.out.println("Geçerli bir not girmediniz");
+			break;
+		}
+
+	}
+
+}
