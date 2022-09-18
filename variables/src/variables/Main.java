1bfe365d516185bfee65bf4609c749882c357b62
alemd <alemd@Computerstafa.home>
2022-09-18 16:54:25 +0300

    "Değişkenler ve Değişken Kullanım ihtiyacı" adlı videonun uygulaması

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Java case sensitive bir dildir yani büyük küçük harfe duyarlıdır
+		int ogreciSayisi=12;
+		String mesaj ="Öğrenci sayısı : ";
+		
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println(mesaj + ogreciSayisi);
+		System.out.println(mesaj + ogreciSayisi);
+	}
+
+}
