45f0bc21e56d03babc2cc4f2b4229f67660177d8
alemd <alemd@Computerstafa.home>
2022-09-23 18:51:00 +0300

    25. dersin uygulaması

--- methods2/src/methods2/Main.java
+++ methods2/src/methods2/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		String mesaj = "Bugün hava çok güzel.";
-		String yeniMesaj = sehirVer();
-		System.out.println(yeniMesaj);
-		int sayi = topla(12,7);
-		System.out.println(sayi);
-		
+		//String yeniMesaj = sehirVer();
+		//System.out.println(yeniMesaj);
+		int toplam = topla2(1,2,3,5,5555,4);
+		System.out.println(toplam);
+
 	}
 
 	public static void ekle() {
@@ -23,9 +23,18 @@ public class Main {
 		System.out.println("Güncellendi");
 	}
 
-	public static int topla(int sayi1,int sayi2) {
+	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 	}
+	
+	public static int topla2(int... sayilar) {
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam =toplam + sayi;
+		}
+		return toplam;
+	}
+
 	public static String sehirVer() {
 		return "ankara";
 	}
